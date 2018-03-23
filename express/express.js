0fe16420cf016ac78ad3223ce4c75b89ab141fdc
camellee13 <camellee13@gmail.com>
2018-03-23 07:20:25 -0400

    change 1

---
+++ express/express.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+
+app.get('/', function(req, res){
+	res.send('Send gogo page!');
+});
+
+
+app.get('/about_me', function(req, res) {
+	res.sendFile('index.html');
+});
+
+app.get('/about', function(req, res) {
+	res.send(JSON.stringify(req.query.name) +', Hello!');
+});
+
+
+app.listen(3000, function (){
+	console.log('Example app is running on port 3000!');
+});
